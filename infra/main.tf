f1779b3d0ebaa0fb76585ec0c23f278d507b978d
Persell Machuca <persell.machuca@gmail.com>
2024-04-09 21:49:43 -0500

    created a new file variables.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,12 @@
 # Create a resource group
 resource "azurerm_resource_group" "azure_rg" {
-  name = "rg-az-resume-python-prod-001"
-  location = "southcentralus"
+  name = "var.resource_group_name"
+  location = "var.location"
 }
 
 # create a storage account
 resource "azurerm_storage_account" "azure-sa" {
-    name                     = "stazpythresume001"
+    name                     = "var.storage_account_name"
     resource_group_name      = azurerm_resource_group.azure_rg.name
     location                 = azurerm_resource_group.azure_rg.location
     account_tier             = "Standard"
---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  description = "The location/region where the Azure resources will be created."
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the Storage Account will be created."
+}
+
+variable "storage_account_name" {
+  description = "the name of the storage account."
+}
+
+variable "" {
+  
+}
