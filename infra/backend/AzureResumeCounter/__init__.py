8065f5240b5b1299b2ea0298f0c3f9a00dadaaa2
Persell Machuca <persell.machuca@gmail.com>
2024-04-27 10:13:03 -0500

    Improved python code, less lines, easier to read

--- infra/backend/AzureResumeCounter/__init__.py
+++ infra/backend/AzureResumeCounter/__init__.py
@@ -1,24 +1,11 @@
 import logging
-
 import azure.functions as func
 
-
-
-# HTTP Request
-def main(req: func.HttpRequest, inputDocument: func.DocumentList,
-         outputDocument: func.Out[func.Document]) -> func.HttpResponse:
+def main(req: func.HttpRequest, inputDocument: func.DocumentList, outputDocument: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    counter = getNewCounterValue(inputDocument[0]['count'])
+    counter = inputDocument[0]['count'] + 1
     inputDocument[0]['count'] = counter
     outputDocument.set(func.Document.from_json(inputDocument[0].to_json()))
-    if counter:
-        return func.HttpResponse(f"{counter}", status_code=200)
-    else:
-        return func.HttpResponse(
-            "Error",
-            status_code=500)
 
-
-def getNewCounterValue(value: int):
-    return value + 1
+    return func.HttpResponse(f"{counter}", status_code=200 if counter else 500)
